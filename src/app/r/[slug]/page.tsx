1b05febc40816e596ef1de112756eac90eebc7cf
Joscha <neske.joscha@gmail.com>
2023-06-14 22:19:10 +0200

    duplicate post fix

--- src/app/r/[slug]/page.tsx
+++ src/app/r/[slug]/page.tsx
@@ -26,7 +26,9 @@ const page = async ({ params }: PageProps) => {
           comments: true,
           subreddit: true,
         },
-
+        orderBy: {
+          createdAt: 'desc'
+        },
         take: INFINITE_SCROLL_PAGINATION_RESULTS,
       },
     },
