4ac063095095b47360358a69698c78a732470944
Rob Harding <robbie.harding@hotmail.com>
2023-06-19 09:03:58 -0400

    use replace instead of onClick

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -4,15 +4,12 @@ import { FC } from 'react'
 import { Icons } from '@/components/Icons'
 import UserAuthForm from '@/components/UserAuthForm'
 import Link from 'next/link'
-import {useRouter} from 'next/navigation';
 
 interface SignInProps {
   isModal?: boolean;
 }
 
 const SignIn: FC<SignInProps> = ({ isModal = false }) => {
-  const router = useRouter();
-
   return (
     <div className='container mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[400px]'>
       <div className='flex flex-col space-y-2 text-center'>
@@ -29,14 +26,8 @@ const SignIn: FC<SignInProps> = ({ isModal = false }) => {
         <Link
           href='/sign-up'
           className='hover:text-brand text-sm underline underline-offset-4'
-          onClick={(e) => {
-            e.preventDefault();
-            if(isModal) {
-              router.replace('/sign-up');
-            } else {
-              router.push('/sign-up');
-            }
-          }}>
+          replace={isModal}
+          }>
           Sign Up
         </Link>
       </p>
--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -1,47 +1,38 @@
 "use client";
 
-import { FC } from 'react'
-import { Icons } from '@/components/Icons'
-import UserAuthForm from '@/components/UserAuthForm'
-import Link from 'next/link'
-import {useRouter} from 'next/navigation'
+import { FC } from "react";
+import { Icons } from "@/components/Icons";
+import UserAuthForm from "@/components/UserAuthForm";
+import Link from "next/link";
 
 interface SignUpProps {
   isModal?: boolean;
 }
 
 const SignUp: Fc<SignUpProps> = ({ isModal = false }) => {
-  const router = useRouter();
-
   return (
-    <div className='container mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[400px]'>
-      <div className='flex flex-col space-y-2 text-center'>
-        <Icons.logo className='mx-auto h-6 w-6' />
-        <h1 className='text-2xl font-semibold tracking-tight'>Sign Up</h1>
-        <p className='text-sm max-w-xs mx-auto'>
+    <div className="container mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[400px]">
+      <div className="flex flex-col space-y-2 text-center">
+        <Icons.logo className="mx-auto h-6 w-6" />
+        <h1 className="text-2xl font-semibold tracking-tight">Sign Up</h1>
+        <p className="text-sm max-w-xs mx-auto">
           By continuing, you are setting up a Breadit account and agree to our
           User Agreement and Privacy Policy.
         </p>
       </div>
       <UserAuthForm />
-      <p className='px-8 text-center text-sm text-muted-foreground'>
-        Already a Breadditor?{' '}
+      <p className="px-8 text-center text-sm text-muted-foreground">
+        Already a Breadditor?{" "}
         <Link
-          href='/sign-in'
-          className='hover:text-brand text-sm underline underline-offset-4'
-          onClick={(e) => {
-            e.preventDefault();
-            if(isModal) {
-              router.replace('/sign-in');
-            } else {
-              router.push('/sign-in');
-            }
-          }}>
+          href="/sign-in"
+          className="hover:text-brand text-sm underline underline-offset-4"
+          replace={isModal}
+        >
           Sign in
         </Link>
       </p>
     </div>
-  )
-}
+  );
+};
 
-export default SignUp
+export default SignUp;
