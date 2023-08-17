a03ddc6c6049220305658889f8ca70fbb1c14bd5
Julio Leon <juliopena143@icloud.com>
2023-08-17 00:11:11 -0600

    navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,5 @@
-const Navbar = async () => {
-    return (<div></div>)
+const Navbar = () => {
+    return (<div className="fixed top-0 inset-x-0 h-fit bg-zinc-100 border-b "></div>)
 }
+
+export default Navbar;
