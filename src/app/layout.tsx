cad217d229c4c79bf89c2693a8086923882c9531
Julio Leon <juliopena143@icloud.com>
2023-08-17 00:02:03 -0600

    nabvar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,13 @@
+import { cn } from '@/lib/utils'
 import '@/styles/globals.css'
+import { Inter } from 'next/font/google'
 
 export const metadata = {
   title: 'Breadit',
   description: 'A Reddit clone built with Next.js and TypeScript.',
 }
+
+const inter = Inter({ subsets: ['latin'] })
 
 export default function RootLayout({
   children,
@@ -11,8 +15,17 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang='en'>
-      <body>{children}</body>
+    <html lang='en'
+      className={cn('bg-white text-slate-900 antialiased light',
+        inter.className
+      )}>
+      <body className='min-h-screen pt-12 bg-slate-50 antialiased'>
+        <Navbar />
+
+        <div className='container max-w-7xl mx-auto h-full pt-12'>
+          {children}
+        </div>
+      </body>
     </html>
   )
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,3 @@
+const Navbar = async () => {
+    return (<div></div>)
+}
