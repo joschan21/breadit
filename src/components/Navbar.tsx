86b937ab5f1c99045b826accd87c7a059955d256
Julio Leon <juliopena143@icloud.com>
2023-08-17 16:10:51 -0600

    nav

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 const Navbar = () => {
-    return (<div className="fixed top-0 inset-x-0 h-fit bg-zinc-100 border-b "></div>)
+    return (<div className="fixed top-0 inset-x-0 h-fit bg-zinc-100 border-b border-zinc-300 z-[10] py-2"></div>)
 }
 
 export default Navbar;
