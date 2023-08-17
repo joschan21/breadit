1c9aa7e9cbae8611bda1735af83931a7293422ff
Julio Leon <juliopena143@icloud.com>
2023-08-17 00:11:24 -0600

    layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { cn } from '@/lib/utils'
 import '@/styles/globals.css'
 import { Inter } from 'next/font/google'
+import Navbar from '@/components/Navbar'
 
 export const metadata = {
   title: 'Breadit',
