df31ae532e307ee973f20a149f1f6397ea6510ab
Julio Leon <juliopena143@icloud.com>
2023-08-21 15:33:29 -0600

    sign in page

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,10 +1,23 @@
+import { buttonVariants } from '@/components/ui/Button'
 import { FC } from 'react'
+import { cn } from '@/lib/utils'
+import Link from 'next/link'
 
 
 const page: FC = () => {
-    return <div className='absolute inset-0'>
-        <div className='h-full max-w-2xl mx-auto flex flex-col items-center'></div>
+    return (<div className='absolute inset-0'>
+        <div className='h-full max-w-2xl mx-auto flex flex-col items-center justify-center gap-20'>
+            <Link
+                href='/'
+                className={cn(buttonVariants({ variant: 'ghost' }),
+                    'self-start -mt-20')}>
+                Home
+            </Link>
+
+            <SignIn />
+        </div>
     </div>
+    )
 }
 
 export default page
---
+++ src/components/SignIn.tsx
@@ -0,0 +1,11 @@
+import { Icons } from "./Icons"
+
+const SignIn = () => {
+    return <div className="container mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[400px]">
+        <div className="flex flex-col space-y-2 text-center">
+            <Icons.logo className="mx-auto h-6 w-6" />
+        </div>
+    </div>
+}
+
+export default SignIn
