c216757cf327475040e1ba9eba6a98d486a784a4
Julio Leon <juliopena143@icloud.com>
2023-08-21 15:08:40 -0600

    wont

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,10 @@
+import { FC } from 'react'
+
+
+const page: FC = () => {
+    return <div className='absolute inset-0'>
+        <div className='h-full max-w-2xl mx-auto flex flex-col items-center'></div>
+    </div>
+}
+
+export default page
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,18 +3,20 @@ import { Icons } from "./Icons";
 import { buttonVariants } from "./ui/Button";
 
 const Navbar = () => {
-    return <div className="fixed top-0 inset-x-0 h-fit bg-zinc-100 border-b border-zinc-300 z-[10] py-2">
-        <div className="container max-w-7xl h-full mx-auto items-center justify-between gap-2">
-            <Link href='/' className="flex gap-2 items-center">
-                <Icons.logo className="h-8 w-8 smh:h-6 sm:w-6" />
-                <p className="hidden text-zinc-700 text-sm font-medium md:block">Breadit</p>
-            </Link>
+    return (
+        <div className="fixed top-0 inset-x-0 h-fit bg-zinc-100 border-b border-zinc-300 z-[10] py-2">
+            <div className="container max-w-7xl h-full mx-auto items-center flex justify-between gap-2">
+                <Link href='/' className="flex gap-2 items-center">
+                    <Icons.logo className="h-8 w-8 smh:h-6 sm:w-6" />
+                    <p className="hidden text-zinc-700 text-sm font-medium md:block">Breadit</p>
+                </Link>
 
-            {/* search bar */}
+                {/* search bar */}
 
-            <Link href="/sign-in" className={buttonVariants()}></Link>
+                <Link href="/sign-in" className={buttonVariants()}>Sign In</Link>
+            </div>
         </div>
-    </div>
+    )
 }
 
 export default Navbar;
