1342ca271320a5b1cd15ab753be167e92a8ea5dc
Julio Leon <juliopena143@icloud.com>
2023-08-21 15:08:32 -0600

    wont commit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,20 @@
+import Link from "next/link";
+import { Icons } from "./Icons";
+import { buttonVariants } from "./ui/Button";
+
 const Navbar = () => {
-    return (<div className="fixed top-0 inset-x-0 h-fit bg-zinc-100 border-b border-zinc-300 z-[10] py-2"></div>)
+    return <div className="fixed top-0 inset-x-0 h-fit bg-zinc-100 border-b border-zinc-300 z-[10] py-2">
+        <div className="container max-w-7xl h-full mx-auto items-center justify-between gap-2">
+            <Link href='/' className="flex gap-2 items-center">
+                <Icons.logo className="h-8 w-8 smh:h-6 sm:w-6" />
+                <p className="hidden text-zinc-700 text-sm font-medium md:block">Breadit</p>
+            </Link>
+
+            {/* search bar */}
+
+            <Link href="/sign-in" className={buttonVariants()}></Link>
+        </div>
+    </div>
 }
 
 export default Navbar;
