0096e7a6c608b659ee6fa23aeeb50ee72938921e
Julio Leon <juliopena143@icloud.com>
2023-08-22 15:16:10 -0600

    user form and icons

--- src/components/UserAuthForm.tsx
+++ src/components/UserAuthForm.tsx
@@ -1,12 +1,39 @@
-import { FC } from 'react'
+"use client"
+
+
+import { FC, useState } from 'react'
 import { Button } from './ui/Button'
 import { cn } from '@/lib/utils'
+import { signIn } from 'next-auth/react';
+import { Icons } from './Icons';
 
 
 const UserAuthForm = () => {
+
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+
+    const loginWithGoogle = async () => {
+        setIsLoading(true);
+
+        try {
+            await signIn('google')
+        } catch (error) {
+
+        } finally {
+            setIsLoading(false)
+        }
+    }
+
     return (
         <div className='flex justify-center'>
-            <Button size='sm' className='w-full'>Google</Button>
+            <Button
+                onClick={loginWithGoogle}
+                isLoading={isLoading}
+                size='sm'
+                className='w-full'>
+                {isLoading ? null : <Icons.google className='h-4 w-4 mr-2' />}
+                Google
+            </Button>
 
         </div>
 
