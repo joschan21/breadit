6ba10a1b3e5eef107bc18811761b0eb1fad89538
Julio Leon <juliopena143@icloud.com>
2023-09-04 17:26:45 -0600

    auth

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,5 @@
+import NextAuth from "next-auth";
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST }
--- src/components/UserAuthForm.tsx
+++ src/components/UserAuthForm.tsx
@@ -18,7 +18,6 @@ const UserAuthForm = () => {
         setIsLoading(true);
 
         try {
-            throw new Error();
             await signIn('google')
         } catch (error) {
             toast({
