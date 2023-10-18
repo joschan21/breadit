5d23d409cf740156573aa45875bac0224d816a7b
Julio Leon <juliopena143@icloud.com>
2023-10-18 16:37:47 -0600

    editor form and route handling

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,10 +1,11 @@
 "use client"
 
-import { FC } from 'react'
+import { FC, useCallback, useRef } from 'react'
 import TextareaAutosize from 'react-textarea-autosize'
 import { useForm } from 'react-hook-form'
 import { PostCreationRequest, PostValidator } from '@/lib/validators/post'
 import { zodResolver } from '@hookform/resolvers/zod'
+import type EditorJS from '@editorjs/editorjs'
 
 interface EditorProps {
     subredditId: string,
@@ -13,7 +14,11 @@ interface EditorProps {
 const Editor: FC<EditorProps> = ({ subredditId }) => {
 
 
-    const { } = useForm<PostCreationRequest>({
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<PostCreationRequest>({
         resolver: zodResolver(PostValidator),
         defaultValues: {
             subredditId,
@@ -21,6 +26,52 @@ const Editor: FC<EditorProps> = ({ subredditId }) => {
             content: null,
         }
     })
+
+    const ref = useRef<EditorJS>()
+
+    const initializeEditor = useCallback(async () => {
+        const EditorJS = (await import('@editorjs/editorjs')).default
+        const Header = (await import('@editorjs/header')).default
+        const Embed = (await import('@editorjs/embed')).default
+        const Table = (await import('@editorjs/table')).default
+        const List = (await import('@editorjs/list')).default
+        const Code = (await import('@editorjs/code')).default
+        const LinkTool = (await import('@editorjs/link')).default
+        const InlineCode = (await import('@editorjs/inline-code')).default
+        const ImageTool = (await import('@editorjs/image')).default
+
+        // !ref.current means not currently being initiated so good to initiate it
+        if (!ref.current) {
+            const editor = new EditorJS({
+                holder: 'editor',
+                onReady() {
+                    ref.current = editor
+                },
+                placeholder: 'Type here to write your post...',
+                inlineToolbar: true,
+                data: { blocks: [] },
+                tools: {
+                    header: Header,
+                    linkTool: {
+                        class: LinkTool,
+                        config: {
+                            endpoint: '/api/link',
+                        }
+                    },
+                    image: {
+                        class: ImageTool,
+                        config: {
+                            uploader: {
+                                async uploadByFile(file: File) {
+
+                                }
+                            }
+                        }
+                    }
+                }
+            })
+        }
+    }, [])
 
 
     return (
